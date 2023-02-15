19dddb8734d113d2e18bd133057f093dd32facef
Armando Serrano <armando.serrano@globant.com>
2023-02-15 17:18:38 -0500

    fix: delete error

--- .github/actions/hello/index.js
+++ .github/actions/hello/index.js
@@ -1,9 +1,10 @@
-
-try {
-    //throw( new Error("some error message"));
     const core=require('@actions/core');
     const github=require('@actions/github');
     const name=core.getInput('who-to-greet');
+try {
+    //throw( new Error("some error message"));
+
+
     core.debug('Debug message: ');
     core.warning('Debug message: ');
     core.error('Error message: ');
@@ -11,7 +12,10 @@ try {
     console.log(`hello ${name}`)
     const time = new Date();
     core.setOutput("time", time.toTimeString());
+    core.startGroup('Loggin github object');
     console.log(JSON.stringify(github),null, '\t');
+    core.endGroup();
+    core.exportVariable('HELLO','hello')
 } catch(error){
     core.setFailed( error.message );
 }
